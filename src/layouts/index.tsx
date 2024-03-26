f16d1fb79e45132716bd1a6a1393bcd08e69957d
André Silveira <andre.silveira1silva@gmail.com>
2024-03-26 13:07:28 -0300

    feat: add app layout

---
+++ src/layouts/index.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router-dom'
+
+import { Header } from '../components/Header'
+
+export function DefaultLayout() {
+  return (
+    <div className="max-w-6xl px-4 mx-auto antialiased">
+      <Header />
+
+      <Outlet />
+    </div>
+  )
+}
