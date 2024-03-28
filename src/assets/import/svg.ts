4589a973c69f0f02e75c5fbed391411c0a540c1e
André Silveira <andre.silveira1silva@gmail.com>
2024-03-28 20:35:57 -0300

    import svg file

---
+++ src/assets/import/svg.ts
@@ -0,0 +1,31 @@
+import espresso from '../espresso.svg'
+import american from '../american.svg'
+import creamyEspresso from '../creamyEspresso.svg'
+import icedCoffee from '../icedCoffee.svg'
+import coffeeWithMilk from '../coffeeWithMilk.svg'
+import latte from '../latte.svg'
+import capuccino from '../capuccino.svg'
+import macchiato from '../macchiato.svg'
+import mochaccino from '../mochaccino.svg'
+import hotChocolate from '../hotChocolate.svg'
+import cuban from '../cuban.svg'
+import hawaiian from '../hawaiian.svg'
+import arabic from '../arabic.svg'
+import irish from '../irish.svg'
+
+export const svg = {
+  espresso,
+  american,
+  creamyEspresso,
+  icedCoffee,
+  coffeeWithMilk,
+  latte,
+  capuccino,
+  macchiato,
+  mochaccino,
+  hotChocolate,
+  cuban,
+  hawaiian,
+  arabic,
+  irish,
+}
