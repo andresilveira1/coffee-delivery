f18726b6d1c777022375e5579c579a0295e51bdd
André Silveira <andre.silveira1silva@gmail.com>
2024-04-10 23:01:31 -0300

    style: update styles

--- src/pages/Home/components/Banner.tsx
+++ src/pages/Home/components/Banner.tsx
@@ -3,7 +3,7 @@ import banner from '../../../assets/banner.svg'
 
 export function Banner() {
   return (
-    <div className="flex gap-14 py-24">
+    <div className="flex flex-col lg:flex-row gap-14 py-14 md:py-24">
       <div>
         <div className="mb-16">
           <h1 className="font-alt font-extrabold text-5xl mb-4 text-base-800">
@@ -16,22 +16,22 @@ export function Banner() {
           </p>
         </div>
 
-        <footer className="flex flex-wrap items-center text-base-700">
-          <span className="flex items-center gap-3 w-[230px] mr-10 mb-5">
+        <footer className="flex flex-wrap gap-2 md:gap-0 items-center text-base-700">
+          <span className="flex items-center gap-3 w-[14.375rem] mr-10 md:mb-5">
             <ShoppingCart
               weight="fill"
               className="w-8 h-8 p-2 text-white bg-yellow-650 rounded-full"
             />
             Compra simples e segura
           </span>
-          <span className="flex items-center gap-3 mb-5">
+          <span className="flex items-center gap-3 md:mb-5">
             <Package
               weight="fill"
               className="w-8 h-8 p-2 text-white bg-base-500 rounded-full"
             />
             Embalagem mantém o café intacto
           </span>
-          <span className="flex items-center gap-3 mr-10">
+          <span className="flex items-center gap-3 md:mr-10">
             <Timer
               weight="fill"
               className="w-8 h-8 p-2 text-white bg-yellow-550 rounded-full"
@@ -48,7 +48,7 @@ export function Banner() {
         </footer>
       </div>
 
-      <img src={banner} alt="" />
+      <img src={banner} alt="" className="w-96 lg:w-auto mx-auto" />
     </div>
   )
 }
