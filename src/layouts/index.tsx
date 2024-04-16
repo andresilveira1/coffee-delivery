78ea8e279aeb09d3080c37ec0d13d9d3d4c5fe70
André Silveira <andre.silveira1silva@gmail.com>
2024-04-15 21:02:08 -0300

    style: add padding bottom

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -4,7 +4,7 @@ import { Header } from '../components/Header'
 
 export function DefaultLayout() {
   return (
-    <div className="max-w-6xl px-4 mx-auto antialiased">
+    <div className="max-w-6xl px-4 pb-4 mx-auto antialiased">
       <Header />
 
       <Outlet />
