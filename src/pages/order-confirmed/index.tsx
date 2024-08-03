1c226f5c05a9178def18967931d2045c3335bdf5
André Silveira <andre.silveira1silva@gmail.com>
2024-08-03 14:47:25 -0300

    feat: add delivery data

--- src/pages/order-confirmed/index.tsx
+++ src/pages/order-confirmed/index.tsx
@@ -1,8 +1,13 @@
-import { CurrencyDollar, MapPin, Timer } from '@phosphor-icons/react'
-import orderConfirmed from '../../assets/orderConfirmed.svg'
+import { useContext } from 'react'
 import { Helmet } from 'react-helmet-async'
+import { CurrencyDollar, MapPin, Timer } from '@phosphor-icons/react'
+
+import { ProductsContext } from '../../contexts/products-contexts'
+import orderConfirmed from '../../assets/orderConfirmed.svg'
 
 export function OrderConfirmed() {
+  const { addressDelivery } = useContext(ProductsContext)
+
   return (
     <main className="mt-48">
       <Helmet title="Pedido confirmado" />
@@ -25,18 +30,42 @@ export function OrderConfirmed() {
                   weight="fill"
                   className="w-8 h-8 p-2 text-white bg-violet-650 rounded-full"
                 />
-                Entrega em <strong></strong>
+                <span>
+                  Entrega em{' '}
+                  <strong>
+                    {addressDelivery?.address}, {addressDelivery?.addressNumber}{' '}
+                  </strong>
+                  {addressDelivery?.district} - {addressDelivery?.city},{' '}
+                  {addressDelivery?.uf}
+                </span>
               </li>
+
               <li className="flex items-center gap-3">
                 <Timer
                   weight="fill"
                   className="w-8 h-8 p-2 text-white bg-yellow-550 rounded-full"
                 />
-                Previsão de entrega <strong>20 min - 30 min</strong>
+                <span>
+                  Previsão de entrega <strong>20 min - 30 min</strong>
+                </span>
               </li>
+
               <li className="flex items-center gap-3">
                 <CurrencyDollar className="w-8 h-8 p-2 text-white bg-yellow-650 rounded-full" />
-                Pagamento na entrega <strong></strong>
+                <span>
+                  Pagamento na entrega{' '}
+                  <strong>
+                    {addressDelivery?.paymentMethod === 'debit'
+                      ? 'Cartão de débito'
+                      : null}
+                    {addressDelivery?.paymentMethod === 'credit'
+                      ? 'Cartão de crédito'
+                      : null}
+                    {addressDelivery?.paymentMethod === 'money'
+                      ? 'Dinheiro'
+                      : null}
+                  </strong>
+                </span>
               </li>
             </ul>
           </div>
